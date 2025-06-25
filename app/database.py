a4843750f2c6887258d4b4bfaa18a99c1b540071
lokesh-22 <lokeshboddupally13@gmail.com>
2025-06-25 16:02:52 +0530

    .

--- app/database.py
+++ app/database.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
